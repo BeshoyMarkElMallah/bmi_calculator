73bd5e2c3451840bdfd6bd11d754065c84a7d066
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2022-08-13 11:40:46 +0200

    first commit

---
+++ lib/components/icon_content.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+class IconContent extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  IconContent({this.text, this.icon});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          text,
+          style: klabelTextStyle,
+        )
+      ],
+    );
+  }
+}
+
